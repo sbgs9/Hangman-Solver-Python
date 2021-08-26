a595420afc1d29c153ba57f6b4ed952feb3af0c6
Ganesh Susarla <ganeshsusarla01@gmail.com>
2021-08-26 01:06:25 -0700

    Fixed main.py to work for situations where first guess is wrong

--- main.py
+++ main.py
@@ -18,14 +18,27 @@ def get_filtered_frequencies(length, doesnt_contain, contains):
     words = open('words.txt', 'r')
     lines = words.readlines()
     freqs = Counter()
-    if len(doesnt_contain) != 0 and len(contains) != 0:
+    if len(doesnt_contain) != 0 and len(contains) == 0:
         for line in lines:
-            if len(line.strip()) == length and any(letter in line.strip() for letter in contains) and not any(letter in line.strip() for letter in doesnt_contain):
+            if len(line.strip()) == length and not any(letter in line.strip() for letter, count in doesnt_contain):
                 freqs += Counter(line.strip())
+    elif len(doesnt_contain) != 0 and len(contains) != 0:
+        for line in lines:
+            if len(line.strip()) == length and any(letter in line.strip() for letter, count in contains) and not any(letter in line.strip() for letter, count in doesnt_contain):
+                for letter,count in contains:
+                    if num_occurrances(line.strip(), letter) == count:
+                        freqs += Counter(line.strip())
     return freqs
+def num_occurrances(word, letter):
+    count = 0
+    for char in word:
+        if char == letter:
+            count += 1
+    return count
 
 def get_indices(word, letter):
     return [i for i, ltr in enumerate(word) if ltr == letter]
+
 # main driver
 def main():
     url = 'https://hangman-api.herokuapp.com/hangman'
@@ -55,23 +68,21 @@ def main():
             guess = requests.put(url, data={'token':token, 'letter': letter})
             print('Guess: ' + letter)
             resp = guess.json()
-            print(resp)
             if resp['correct'] == False:
-                doesnt_contain.append(letter)
-                print('Guess was wrong')
+                doesnt_contain.append((letter, 0))
+                #print('Guess was wrong')
                 num_incorrect += 1
                 break
             elif resp['correct'] == True:
-                contains.append(letter)
-                print(get_indices(resp['hangman'], letter))
+                contains.append((letter, len(get_indices(resp['hangman'], letter))))
                 for index in get_indices(resp['hangman'], letter):
                     word_preview = word_preview[:index] + letter + word_preview[index+1:]
                 print(word_preview)
-                print('Guess was correct')
+                #print('Guess was correct')
     if num_incorrect == 7:
         print("Guess we couldn't solve this one! We'll get it next time!")
     else:
-        print('The correct word is: ' + word_preview)
+        print('WE GOT IT! THE CORRECT WORD IS: ' + word_preview)
         
 
 if __name__ == '__main__':
